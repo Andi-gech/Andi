fe9d6453c269e617989bfde0d257a3a1becf57c6
Andi-gech <andi.fab23@gmail.com>
2022-12-03 10:30:14 -0500

    first commit

--- src/banner.js
+++ src/banner.js
@@ -22,13 +22,28 @@ function Banner(props) {
   };
 const ZoomInScrollOut = batch(FadeIn(), ZoomIn());
 const FadeUp = batch(Fade(), MoveIn(),);
+const [width, setWidth] = useState<number>(window.innerWidth);
+
+function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+}
+useEffect(() => {
+    window.addEventListener('resize', handleWindowSizeChange);
+    return () => {
+        window.removeEventListener('resize', handleWindowSizeChange);
+    }
+}, []);
+
+const isMobile = width <= 768;
 
 
   return (
     <ScrollContainer>
       <div className="banner">
-       
-        <Navbars />
+       if ({isMobile}) {
+          <Navbars />
+       }
+      
       
         
 
