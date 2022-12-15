6e94052eaaae235beab17589182f18858328d33c
Andi-gech <andi.fab23@gmail.com>
2022-12-15 10:04:51 -0500

    first

--- src/myprofile.js
+++ src/myprofile.js
@@ -7,7 +7,7 @@ function Myprofile(props) {
     <div className="Profile" id="profile212">
       <img src={PRO} alt="arge" />
       <p>
-        &nbsp; &nbsp; Hello and welcome to my personal website. I'm Andualem Getachew, to be exact I'm a third-year defense engineering student. Additionally, I'm a student of the Alx Software Engineering Program.
+        &nbsp; &nbsp; Hello and welcome to my personal Portfolio. I'm Andualem Getachew, to be exact I'm a third-year defense engineering student. Additionally, I'm a student of the Alx Software Engineering Program.
       </p>
     </div>
   );
